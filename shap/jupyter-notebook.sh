0e353611cc62042d5adf27ce9e2d842936c76f21
jiheeyy <86678801+jiheeyy@users.noreply.github.com>
2023-09-23 20:21:47 -0500

    jupyter add

---
+++ shap/jupyter-notebook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+unset XDG_RUNTIME_DIR
+NODEIP=$(hostname -i)
+NODEPORT=$(( $RANDOM + 1024))
+echo "ssh command: ssh -N -L 8888:$NODEIP:$NODEPORT `jiheeyou`@fe01.ai.cs.uchicago.edu"
+. ~/myenv/bin/activate
+jupyter-notebook --ip=$NODEIP --port=$NODEPORT --no-browser
